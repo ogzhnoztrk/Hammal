a8189bd0f4e77079faa372f3e2ae472566dc6ab9
Mehmet Toy <mehmet.toy@pronovuma.com>
2023-04-02 22:50:15 +0300

    First commit

---
+++ Hammal.DataAccess/Migrations/20230402194731_updateDb.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hammal.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
