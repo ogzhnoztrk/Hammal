56e46701d681c085f3e525210035f63246b3b132
oguzhannx <oguzhanoztrk00@gmail.com>
2023-04-07 14:24:10 +0300

    .

--- Hammal.Utilities/EmailSender.cs
+++ Hammal.Utilities/EmailSender.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,10 +13,23 @@ namespace Hammal.Utilities
 {
     public class EmailSender : IEmailSender
     {
-        public Task SendEmailAsync(string email, string subject, string htmlMessage)
-        {
-            return Task.CompletedTask;
-        }
+		public string? SendGridSecret { get; set; }
+		public EmailSender(IConfiguration _config)
+		{
+			SendGridSecret = _config.GetValue<string>("SendGrid:SecretKey");
+		}
 
-    }
+		
+
+		public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+			var client = new SendGridClient(SendGridSecret);
+			var from = new EmailAddress("hammal@support.com", "Hammal");
+			var to = new EmailAddress(email);
+			var msg = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+			return client.SendEmailAsync(msg);
+
+		}
+
+	}
 }
