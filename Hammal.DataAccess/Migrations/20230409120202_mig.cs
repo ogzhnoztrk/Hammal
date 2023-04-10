82f2d5d3df9e1452ef04f2946f2bfbeb74e6e25c
oguzhannx <oguzhanoztrk00@gmail.com>
2023-04-10 18:19:42 +0300

    sayfa düzenlemeleri yapıldı

---
+++ Hammal.DataAccess/Migrations/20230409120202_mig.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hammal.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Aciklama",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImgPath",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Aciklama",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "ImgPath",
+                table: "Categories");
+        }
+    }
+}
